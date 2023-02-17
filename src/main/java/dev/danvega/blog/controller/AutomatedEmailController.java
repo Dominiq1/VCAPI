db6ac0f216b2cbb44ac29e66438bee6fc3ebf97a
dominiq1 <dominiqmartinez13@gmail.com>
2023-02-16 16:45:40 -0800

    voltaic

--- src/main/java/dev/danvega/blog/controller/AutomatedEmailController.java
+++ src/main/java/dev/danvega/blog/controller/AutomatedEmailController.java
@@ -1,45 +1,45 @@
 package dev.danvega.blog.controller;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import  org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//import java.util.Properties;
-//import java.util.concurrent.atomic.AtomicLong;
-//@RestController
-//public class AutomatedEmailController {
-//
-//    private static final String template = "Sent an email to %s! It should be there soon.";
-//    private final AtomicLong counter = new AtomicLong();
-//
-//    private final JavaMailSender emailSender;
-//
-//    public AutomatedEmailController(JavaMailSender emailSender) {
-//        this.emailSender = emailSender;
-//    }
-//
-//    @GetMapping("/email")
-//    public Greeting email(@RequestParam String email) {
-//
-//        // Create and send email
-//        SimpleMailMessage message = new SimpleMailMessage();
-//        message.setTo(email);
-//        message.setSubject("New Task!");
-//        message.setFrom("dominiq@unhashlabs.io");
-//        message.setText("Hey there! You have a new task!");
-//
-//        emailSender.send(message);
-//        // Return response
-//        return new Greeting(counter.incrementAndGet(), String.format(template, email));
-//    }
-//
-//}
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import  org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicLong;
+@RestController
+public class AutomatedEmailController {
+
+    private static final String template = "Sent an email to %s! It should be there soon.";
+    private final AtomicLong counter = new AtomicLong();
+
+    private final JavaMailSender emailSender;
+
+    public AutomatedEmailController(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @GetMapping("/email")
+    public Greeting email(@RequestParam String email) {
+
+        // Create and send email
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("New Task!");
+        message.setFrom("dominiq@unhashlabs.io");
+        message.setText("Hey there! You have a new task!");
+
+        emailSender.send(message);
+        // Return response
+        return new Greeting(counter.incrementAndGet(), String.format(template, email));
+    }
+
+}
 //
--- src/main/java/dev/danvega/blog/controller/MailConfig.java
+++ src/main/java/dev/danvega/blog/controller/MailConfig.java
@@ -1,44 +1,44 @@
 package dev.danvega.blog.controller;
-//
-//        import org.springframework.beans.factory.annotation.Value;
-//        import org.springframework.context.annotation.Bean;
-//        import org.springframework.context.annotation.Configuration;
-//        import org.springframework.mail.javamail.JavaMailSender;
-//        import org.springframework.mail.javamail.JavaMailSenderImpl;
-//        import java.util.Properties;
-//
-//
-//@Configuration
-//public class MailConfig {
-//
-//    @Value("${spring.mail.host}")
-//    private String host;
-//
-//    @Value("${spring.mail.port}")
-//    private int port;
-//
-//    @Value("${spring.mail.username}")
-//    private String username;
-//
-//    @Value("${spring.mail.password}")
-//    private String password;
-//
-//    @Bean
-//    public JavaMailSender javaMailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.porkbun.com");
-//        mailSender.setPort(587);
-//
-//        mailSender.setUsername("Dominiq@unhashlabs.io");
-//        mailSender.setPassword("Chinoman11");
-//
-//        Properties props = mailSender.getJavaMailProperties();
-//        props.put("mail.smtp.host", "smtp.porkbun.com");
-//        props.put("mail.smtp.port", "587");
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//
-//
-//        return mailSender;
-//    }
-//}
+
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.mail.javamail.JavaMailSender;
+        import org.springframework.mail.javamail.JavaMailSenderImpl;
+        import java.util.Properties;
+
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.porkbun.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("Dominiq@unhashlabs.io");
+        mailSender.setPassword("Chinoman11");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.host", "smtp.porkbun.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+
+        return mailSender;
+    }
+}
